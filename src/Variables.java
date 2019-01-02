db6d37a4bccdad4934164548bc45fb77580310d8
Luis Jimenez <luisjimenez@Luiss-MacBook-Pro.local>
2019-01-02 13:53:10 -0500

    Variables finished

--- src/Variables.java
+++ src/Variables.java
@@ -19,21 +19,39 @@ public class Variables {
 
   }
 
-  public String printHelloWorld(boolean trueOrFalse){
+  public String printHelloWorld(boolean trueOrFalse) {
 
     String isTrue;
     String isFalse;
 
-    if (trueOrFalse == true){
+    if (trueOrFalse == true) {
       isTrue = "Hello World!";
       return isTrue;
     } else isFalse = "There is no spoon";
     return isFalse;
   }
 
+  public static greatestCommonFactor(int num1, int num2) {
+
+
+    int gcf = 1;
+    int k = 2;
+
+    while (k <= num1 && k <= num2) {
+      if (num1 % k == 0 && num2 % k == 0) {
+      }
+      gcd = k;
+      k++;
+    }
+    System.out.println("The Greatest Common Factor for " + num1 + " and " + num2 + " is " + gcf);
+  }
+
+
+
   public static void main(String args[]) {
 
     isOdd(2);
     printHelloWorld(true);
+    greatestCommonFactor(12,2);
   }
 }
