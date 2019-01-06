f3b1593593843427328f5065bd6e7c3e610a6e54
Luis Jimenez <luisjimenez@Luiss-MacBook-Pro.local>
2019-01-05 19:09:20 -0500

    Methods done

--- src/Methods.java
+++ src/Methods.java
@@ -1,19 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
 
 public class Methods {
 
-  public int calculateSquare (int num1){
+  public int calculateSquare(int num1) {
 
-    int squarednumber = num1 * num1;
+    int squaredNumber;
+    squaredNumber = num1 * num1;
 
-    return squarednumber;
+    return squaredNumber;
   }
-  public int calculateSquareRoot (int num1){
 
-  int squareRoot = Math.sqrt(num1);
+  public static  int calculateSquareRoot(int num1) {
 
-  return squareRoot;
+    return (int) Math.sqrt(num1);
   }
-  public boolean isMultiple (int num1, int num2){
+
+  public static String toLowerCase(String str) {
+
+    return str.toLowerCase();
+  }
+
+  public static boolean isMultiple(int num1, int num2) {
 
     if (num2 % num1 == 0) {
       return true;
@@ -21,8 +29,29 @@ public class Methods {
     return false;
   }
 
+  private static void prettyInteger(int num1){
 
-  public static void main (String args[]) {
+    char c = '*';
+    char [] chars = new char[num1];
+    Arrays.fill(chars,c);
+    System.out.println(String.valueOf(chars) + " "+ num1 + " "+  String.valueOf(chars));
+
+  }
+
+  private static int random (int min, int max){
+
+    int diff = max - min;
+    Random rn = new Random();
+    int i = rn.nextInt(diff + 1);
+    i += min;
+    System.out.print(i);
+    return i;
+  }
+
+  public static void main(String args[]) {
+
+    prettyInteger(8);
+    random(6,25);
 
   }
 }
